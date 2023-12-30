ab7bdeeb4f3b5b5c739f8fbf163535a61b130532
harbindersingh621@gmail.com <harbindersingh621@gmail.com>
2023-12-30 23:43:21 +0530

    3 Modules completed

--- week-3/01-middlewares/02-ratelimitter.js
+++ week-3/01-middlewares/02-ratelimitter.js
@@ -1,5 +1,3 @@
-const request = require('supertest');
-const assert = require('assert');
 const express = require('express');
 const app = express();
 // You have been given an express server which has a few endpoints.
@@ -10,11 +8,26 @@ const app = express();
 // User will be sending in their user id in the header as 'user-id'
 // You have been given a numberOfRequestsForUser object to start off with which
 // clears every one second
-
+app.use(express.json());
 let numberOfRequestsForUser = {};
 setInterval(() => {
     numberOfRequestsForUser = {};
 }, 1000)
+
+app.use((req, res, next)=>{
+  const userId = req.headers["user-id"];
+  if(numberOfRequestsForUser[userId]){
+    numberOfRequestsForUser[userId] += 1;
+    if(numberOfRequestsForUser[userId] > 5){
+      return res.status(404).send("Refresh too frequently");
+    }else{
+      next();
+    }
+  }else{
+    numberOfRequestsForUser[userId]= 1;
+    next();
+  }
+});
 
 app.get('/user', function(req, res) {
   res.status(200).json({ name: 'john' });
--- week-3/03-mongo/routes/admin.js
+++ week-3/03-mongo/routes/admin.js
@@ -1,18 +1,54 @@
 const { Router } = require("express");
 const adminMiddleware = require("../middleware/admin");
+const {Admin, Course} = require("../db/index.js")
 const router = Router();
 
 // Admin Routes
 app.post('/signup', (req, res) => {
     // Implement admin signup logic
+    const username = req.body.username;
+    const password = req.body.password;
+    
+    Admin.create({username: username, 
+        password : password})
+    .then(()=>{
+        res.status(200).json({
+            message: 'Admin created successfully'
+        });
+    }).catch((err)=>{
+        res.status(411).send(err)
+    });
 });
 
 app.post('/courses', adminMiddleware, (req, res) => {
     // Implement course creation logic
+    const title = req.body.title;
+    const description = req.body.description;
+    const price = req.body.price;
+    const imageLink = req.body.imageLink;
+
+    const newCourse = Course.create({
+        title: title,
+        description: description,
+        price: price,
+        imageLink: imageLink
+    }).then(()=>{
+      res.status(200).json({message: 'Course created successfully', courseId: newCourse._id});
+    }).catch((err)=>{
+        res.status(411).send(err);
+    });
 });
 
-app.get('/courses', adminMiddleware, (req, res) => {
+app.get('/courses', adminMiddleware, async (req, res) => {
     // Implement fetching all courses logic
+    const allCourses = await Course.find({});
+    if(allCourses){
+    res.status(200).json({
+        courses : allCourses
+    });
+}else{
+    res.status(410).send("Error while sending courses");
+}
 });
 
 module.exports = router;
